0e2f0736d2c4e3579e6a612ab3226dc0265c9d73
roman <ronnoverro@gmail.com>
2023-06-20 13:31:53 -0700

    all 3 steps and audio plays successful

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const startCtxBtn = document.querySelector(".start");
 const setupSamplesBtn = document.querySelector(".setup-samples");
 const playSampleBtn = document.querySelector(".play-sample");
 
-const samplePaths = ["./audio/explosionLoud.mp3", "./audio/firework1 copy.mp3", "./audio/laser.mp3"];
+const samplePaths = ["./audio/explosionLoud.mp3", "./audio/laser.mp3"];
 
 startCtxBtn.addEventListener("click", () => {
     audioContext = new AudioContext();
@@ -19,9 +19,11 @@ setupSamplesBtn.addEventListener("click", () => {
     setupSamples(samplePaths).then((response) => {
         // our globally accessable "samples" var now contains the audio buffers!
         samples = response; 
+        // this returns an array of fulfilled promises instead of an array of AudioBuffers
         console.log(samples);
         // a nested addEventListener! For actually playing the sample:
         playSampleBtn.addEventListener("click", () => {
+            playSample(samples[1], 0);
         });
     })
 });
@@ -29,6 +31,7 @@ setupSamplesBtn.addEventListener("click", () => {
 /* we're gonna have to call the fetch api with the file path and get an array buffer from that.
 because we have to wait (calling this func from elsewhere), we need to declare it as an async func. */
 
+// THIS IS GOOD:
 async function getFile(filePath) {
     // response returns the raw data to us
     const response = await fetch(filePath);
@@ -39,6 +42,8 @@ async function getFile(filePath) {
     return audioBuffer;
 }
 
+
+// THIS IS GOOD:
 // func to setup audio files we need to load. Passed an array:
 async function setupSamples(paths) {
     console.log("setting up samples...");
@@ -46,17 +51,20 @@ async function setupSamples(paths) {
 
     // why don't we use normal for loop? we use a "for of" loop because of our awaiting:
     for (const path of paths) {
-        const sample = getFile(path);
+        // w/p the "await" here, it would just return an array of promises instead of an array of audio buffers!:
+        const sample = await getFile(path);
         audioBuffers.push(sample);
     }
     console.log("done.");
     return audioBuffers;
 }
 
+// THIS IS GOOD:
 // now a func. for actually playing the audio:
 function playSample(audioBuffer, time) {
     const sampleSource = audioContext.createBufferSource();
-    sampleSource.buffer = audioBuffer   // declare sampleSource's buffer (audio itself)
-    sampleSource.connect(audioContext.destination)  // audioContext.destination is the speakers
+    // ERROR IS HERE:
+    sampleSource.buffer = audioBuffer;   // declare sampleSource's buffer (audio itself)
+    sampleSource.connect(audioContext.destination);  // audioContext.destination is the speakers
     sampleSource.start(time);   // play audio!
 }
